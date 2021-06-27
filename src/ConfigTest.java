841a658205494386b25d530580100905cf79075b
Elorza Jimenez, Joel <jjimenez60@gatech.edu>
2021-06-27 05:32:39 -0400

    Update ConfigTest with more tests for configuration screen

--- src/ConfigTest.java
+++ src/ConfigTest.java
@@ -13,22 +13,32 @@ import static org.testfx.api.FxAssert.verifyThat;
 
 public class ConfigTest extends ApplicationTest {
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(final Stage primaryStage) throws Exception {
         Main dungeon16 = new Main();
         dungeon16.start(primaryStage);
-        clickOn(893.0, 547.0); //hit start
+        dungeon16.startConfigScreen();
     }
-
+    /**
+     * Test that ensures configuration screen loads correctly.
+     **/
     @Test
     public void testConfigScreen() {
         //check that our scene is config screen scene
-        verifyThat(window("Dungeon 16 Config Screen"), WindowMatchers.isShowing());
+        verifyThat(window("Dungeon 16 Config Screen"),
+                WindowMatchers.isShowing());
     }
-    
+
+    /**
+     * Tests basic input in all configuration
+     * fields: name, difficulty, and weapon.
+     * Ensures this will allow screen to change
+     * to first room after clicking on "Continue".
+     **/
     @Test
     public void basicInputTest() {
         moveTo(point("Enter Name..."));
-        clickOn(MouseButton.PRIMARY).type(KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D);
+        clickOn(MouseButton.PRIMARY).type(KeyCode.W, KeyCode.A,
+                KeyCode.S, KeyCode.D);
         clickOn("Difficulty?");
         clickOn("Hard");
         clickOn("Weapon?");
@@ -37,7 +47,11 @@ public class ConfigTest extends ApplicationTest {
         verifyThat("wasd", NodeMatchers.isNotNull());
         verifyThat("Magic", NodeMatchers.isNotNull());
     }
-    
+    /**
+     * Test checks that an error window appears
+     * after setting difficulty and weapon, but
+     * leaving name empty.
+     **/
     @Test
     public void validNameInputTest() {
         clickOn("Difficulty?");
@@ -52,6 +66,47 @@ public class ConfigTest extends ApplicationTest {
         clickOn("Broadsword");
         clickOn("Magic");
         moveBy(0, 360, Motion.DIRECT);
+        clickOn(MouseButton.PRIMARY);
+        verifyThat(window("Error!"), WindowMatchers.isShowing());
+    }
+    /**
+     * Test checks that an error window appears
+     * after setting name and weapon, but
+     * leaving difficulty empty.
+     **/
+    @Test
+    public void emptyDifficultyTest() {
+        moveTo(point("Enter Name..."));
+        clickOn(MouseButton.PRIMARY).type(KeyCode.J, KeyCode.A, KeyCode.Y);
+        clickOn("Weapon?");
+        clickOn("Magic");
+        moveBy(0, 360, Motion.DIRECT);
+        clickOn(MouseButton.PRIMARY);
+        verifyThat(window("Error!"), WindowMatchers.isShowing());
+    }
+    /**
+     * Test checks that an error window appears
+     * after setting name and difficulty, but
+     * leaving weapon empty.
+     **/
+    @Test
+    public void emptyWeaponTest() {
+        moveTo(point("Enter Name..."));
+        clickOn(MouseButton.PRIMARY).type(KeyCode.J, KeyCode.A, KeyCode.Y);
+        clickOn("Difficulty?");
+        clickOn("Medium");
+        moveBy(0, 410, Motion.DIRECT);
+        clickOn(MouseButton.PRIMARY);
+        verifyThat(window("Error!"), WindowMatchers.isShowing());
+    }
+    /**
+     * Test checks that an error window appears
+     * after name, difficulty, weapon is empty.
+     **/
+    @Test
+    public void emptyConfigTest() {
+        moveTo(point("Enter Name..."));
+        moveBy(0, 500, Motion.DIRECT);
         clickOn(MouseButton.PRIMARY);
         verifyThat(window("Error!"), WindowMatchers.isShowing());
     }
