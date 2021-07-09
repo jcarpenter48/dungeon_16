40f01bbfaad670653f6c26edbec22a9e3d07ea73
Teng, David Samuel <dteng8@gatech.edu>
2021-07-09 09:48:41 -0500

    Changed roomtile class to use entities instead of temporary variables

--- src/RoomTile.java
+++ src/RoomTile.java
@@ -1,7 +1,6 @@
 public class RoomTile {
     private String entityType;
-    private Chest chest;
-    private Enemy enemy;
+    private Entity tileEntity
     private boolean playerLocated;
     
     public RoomTile (String entity) {
@@ -10,15 +9,31 @@ public class RoomTile {
         } else if (entity == "Chest") {
             double rngFactor = Math.random();
             if rngFactor < 0.95 {
-            chest = new Chest(Math.random() * 10, null);
+            tileEntity = new Chest((int) (Math.random() * 10), null, false);
             } else {
                 int weaponRange = (int) ((Math.random() * 3) + 1);
                 int weaponDamage = (int) ((Math.random() * 3) + 1);
                 Weapon temp = new Weapon (weaponRange, weaponDamage);
                 chest = new Chest(Math.random() * 10, temp);
             }
+            entityType == "Chest";
+        //} else if (entity == "Hazard") {
+        //     //haxzard code
+        } else if (entity == "DoorUp") {
+	    tileEntity = new Door("Up", true);
+            entityType = "Door";
+        } else if (entity == "DoorDown") {
+	    tileEntity = new Door("Down", true);
+	    entityType = "Door";
+        } else if (entity == "DoorRight") {
+	    tileEntity = new Door("Right", true);
+	    entityType = "Door";
+        } else if (entity == "DoorLeft") {
+	    tileEntity = new Door("Left", true);
+	    entityType = "Door";
         } else {
              //entity isEnemyCode
+             break;
         }
     }
     public void clearTile () {
@@ -26,5 +41,12 @@ public class RoomTile {
         chest = null;
         enemy = null;
         playerLocated = nulll;
+    }
+    public boolean entityLocated() {
+        if (playerLocated == null && entityType == null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
