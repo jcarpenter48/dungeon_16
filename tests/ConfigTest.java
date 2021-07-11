2dbd4a262e0cea25d4744104f2bab57d05a7f1a7
Ian Carpenter <ccs_iancarpenter@gatech.edu>
2021-07-11 13:43:03 -0400

    Add Hardik's tests, refactor

---
+++ tests/ConfigTest.java
@@ -0,0 +1,119 @@
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.matcher.base.NodeMatchers;
+import javafx.stage.Stage;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.KeyCode;
+import org.testfx.matcher.base.WindowMatchers;
+import org.testfx.robot.Motion;
+
+import static org.testfx.api.FxAssert.verifyThat;
+
+public class ConfigTest extends ApplicationTest {
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        view.Main dungeon16 = new view.Main();
+        dungeon16.start(primaryStage);
+        clickOn(893.0, 547.0); //hit start
+    }
+
+    @Test
+    public void testEmptyTextField() {
+        moveTo(point("Enter Name..."));
+        clickOn(MouseButton.PRIMARY);
+        moveBy(0, 500);
+    }
+
+    /**
+     * Test that ensures configuration screen loads correctly.
+     **/
+    @Test
+    public void testConfigScreen() {
+        //check that our scene is config screen scene
+        verifyThat(window("Dungeon 16 Config Screen"),
+                WindowMatchers.isShowing());
+    }
+
+    /**
+     * Tests basic input in all configuration
+     * fields: name, difficulty, and weapon.
+     * Ensures this will allow screen to change
+     * to first room after clicking on "Continue".
+     **/
+    @Test
+    public void basicInputTest() {
+        moveTo(point("Enter Name..."));
+        clickOn(MouseButton.PRIMARY).type(KeyCode.W, KeyCode.A,
+                KeyCode.S, KeyCode.D);
+        clickOn("Difficulty?");
+        clickOn("Hard");
+        clickOn("Weapon?");
+        clickOn("Magic");
+        verifyThat("Hard", NodeMatchers.isNotNull());
+        verifyThat("wasd", NodeMatchers.isNotNull());
+        verifyThat("Magic", NodeMatchers.isNotNull());
+    }
+    /**
+     * Test checks that an error window appears
+     * after setting difficulty and weapon, but
+     * leaving name empty.
+     **/
+    @Test
+    public void validNameInputTest() {
+        clickOn("Difficulty?");
+        clickOn("Medium");
+        clickOn("Weapon?");
+        clickOn("Broadsword");
+        moveBy(0, 360, Motion.DIRECT);
+        clickOn(MouseButton.PRIMARY);
+        verifyThat(window("Error!"), WindowMatchers.isShowing());
+        moveTo(point("Enter Name..."));
+        clickOn(MouseButton.PRIMARY).type(KeyCode.SPACE, KeyCode.SPACE);
+        clickOn("Broadsword");
+        clickOn("Magic");
+        moveBy(0, 360, Motion.DIRECT);
+        clickOn(MouseButton.PRIMARY);
+        verifyThat(window("Error!"), WindowMatchers.isShowing());
+    }
+    /**
+     * Test checks that an error window appears
+     * after setting name and weapon, but
+     * leaving difficulty empty.
+     **/
+    @Test
+    public void emptyDifficultyTest() {
+        moveTo(point("Enter Name..."));
+        clickOn(MouseButton.PRIMARY).type(KeyCode.J, KeyCode.A, KeyCode.Y);
+        clickOn("Weapon?");
+        clickOn("Magic");
+        moveBy(0, 360, Motion.DIRECT);
+        clickOn(MouseButton.PRIMARY);
+        verifyThat(window("Error!"), WindowMatchers.isShowing());
+    }
+    /**
+     * Test checks that an error window appears
+     * after setting name and difficulty, but
+     * leaving weapon empty.
+     **/
+    @Test
+    public void emptyWeaponTest() {
+        moveTo(point("Enter Name..."));
+        clickOn(MouseButton.PRIMARY).type(KeyCode.J, KeyCode.A, KeyCode.Y);
+        clickOn("Difficulty?");
+        clickOn("Medium");
+        moveBy(0, 410, Motion.DIRECT);
+        clickOn(MouseButton.PRIMARY);
+        verifyThat(window("Error!"), WindowMatchers.isShowing());
+    }
+    /**
+     * Test checks that an error window appears
+     * after name, difficulty, weapon is empty.
+     **/
+    @Test
+    public void emptyConfigTest() {
+        moveTo(point("Enter Name..."));
+        moveBy(0, 500, Motion.DIRECT);
+        clickOn(MouseButton.PRIMARY);
+        verifyThat(window("Error!"), WindowMatchers.isShowing());
+    }
+}
---
+++ tests/TitleTest.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+import javafx.stage.Stage;
+import org.testfx.matcher.base.WindowMatchers;
+import view.Main;
+
+import static org.testfx.api.FxAssert.verifyThat;
+
+public class TitleTest extends ApplicationTest {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Main dungeon16 = new Main();
+        dungeon16.start(primaryStage);
+    }
+
+    @Test
+    public void testTitleScreen() {
+        verifyThat(window("Dungeon 16 Title Screen"), WindowMatchers.isShowing());
+    }
+
+    @Test
+    public void testStartEvent() {
+        verifyThat(window("Dungeon 16 Title Screen"), WindowMatchers.isShowing());
+        // Start Button
+        clickOn(893.0, 547.0);
+        // Verify new screen successfully loaded
+        verifyThat(window("Dungeon 16 Config Screen"), WindowMatchers.isShowing());
+    }
+
+    @Test
+    public void testStartBtn() {
+        clickOn(893.0, 547.0);
+        //check that we moved to the player config screen after hitting start
+        verifyThat(window("Dungeon 16 Config Screen"), WindowMatchers.isShowing());
+    }
+}
