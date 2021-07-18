2941548288671080e289b0e84087c160393ce29e
Teng, David Samuel <dteng8@gatech.edu>
2021-07-18 13:03:31 -0500

    Added Populate Room

--- src/Room.java
+++ src/Room.java
@@ -1,45 +1,87 @@
+import java.net.URI;
+import java.io.File;
+// this is for images and background
+import javafx.scene.image.Image;
+//import javafx.scene.image.ImageView;
+import javafx.scene.layout.BackgroundImage;
+//import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundPosition;
+import java.util.*;
+
 public class Room {
     private Room left;
     private Room right;
     private Room up;
     private Room down;
     private RoomTile[][] tiles;
+    private boolean isRoomExit = false;
+    private List<Enemy> enemyList;
+    private String roomVariant = "room_"; //you must set room adjacents in udlr order!
+    
+    private File srcPathFile = new File(System.getProperty("user.dir"));
+    //private File relativePathFile = new File(srcPathFile.getParent() + "");
+    private URI relativePath = srcPathFile.toURI();
+    private String roomBackground = (relativePath
+            + "/res/environments/");
 
     public Room() {
-        tiles = (RoomTile[][]) new Object[9][9];
+        enemyList = new ArrayList<>();
+        tiles = (RoomTile[][]) new RoomTile[9][9];
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 tiles[i][j] = new RoomTile(null);
             }
         }
+        int x = (int) (Math.random() * 3 + 1);
     }
-    public RoomTile retRoomTile(int y, int x) {
-        return tiles[y][x];
-    }
-    public void setLeft(Room x) {
-        left = x;
-        if (x != null) {
-            tiles[5][0] = new RoomTile("DoorLeft");
+    public Room(int x) {
+        enemyList = new ArrayList<>();
+        tiles = (RoomTile[][]) new RoomTile[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                tiles[i][j] = new RoomTile(null);
+            }
         }
+        populateRoom(x);
     }
-    public void setRight(Room x) {
-        right = x;
-        if (x != null) {
-            tiles[5][8] = new RoomTile("DoorRight");
-        }
+    public RoomTile retRoomTiles(int x, int y) {
+        return tiles[x][y];
+    }
+    public void setRoomVariant(String newVariant) {
+        this.roomVariant = newVariant;
     }
     public void setUp(Room x) {
         up = x;
-        if (x != null) {
+        if (up != null) {
             tiles[0][5] = new RoomTile("DoorUp");
+            roomVariant = roomVariant + "u";
         }
     }
     public void setDown(Room x) {
         down = x;
-        if (x != null) {
+        if (down != null) {
             tiles[8][5] = new RoomTile("DoorDown");
+            roomVariant = roomVariant + "d";
         }
     }
+    public void setLeft(Room x) {
+        left = x;
+        if (left != null) {
+            tiles[5][0] = new RoomTile("DoorLeft");
+            roomVariant = roomVariant + "l";
+        }
+    }
+    public void setRight(Room x) {
+        right = x;
+        if (right != null) {
+            tiles[5][8] = new RoomTile("DoorRight");
+            roomVariant = roomVariant + "r";
+        }
+    }
+    public void setExit(boolean isSet) {
+        isRoomExit = isSet;
+    }
+
     public Room retUp() {
         return this.up;
     }
@@ -52,7 +94,38 @@ public class Room {
     public Room retRight() {
         return this.right;
     }
-    //public void populateRoom () {
-    //    
-    //}
+    public String retRoomVariant() {
+        return roomVariant;
+    }
+    public boolean isExit() {
+        return isRoomExit;
+    }
+    
+    public BackgroundImage createBackground() {
+        System.out.println("Background Name: " + roomBackground + roomVariant + ".png");
+        try {
+            Image backgroundImage = new Image(roomBackground + roomVariant + ".png");
+            BackgroundImage fullBackground = new BackgroundImage(backgroundImage,
+                        null, null, BackgroundPosition.CENTER, null);
+            return fullBackground;
+        } catch (Exception eex) {
+            System.out.println("Background Image not Found or Invalid");
+        }
+        return null; //self explanatory method
+    }
+
+    public void populateRoom (int x) {
+        for (int i = 0; i < x; i++) {
+            int range = (int) (Math.random() * 3 + 1);
+            int randomizeX = (int) (Math.random() * 7 + 1);
+            int randomizeY = (int) (Math.random() * 7 + 1);
+            while (retRoomTiles(randomizeX, randomizeY).entityLocated() == false) {
+                int randomizeX = (int) (Math.random() * 7 + 1);
+                int randomizeY = (int) (Math.random() * 7 + 1);
+            }
+            Enemy tempEnemy = new Enemy(range, randomizeX, randomizeY, false);
+            retRoomTiles(randomizeX, randomizeY).setEnemy(tempEnemy);
+            enemyList.add(tempEnemy);
+        }
+    }
 }
