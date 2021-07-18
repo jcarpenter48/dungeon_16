38c920490dd51830708c9b571a387c8f45b16300
Teng, David Samuel <dteng8@gatech.edu>
2021-07-18 13:03:11 -0500

    Update MapGenerator.java

--- src/MapGenerator.java
+++ src/MapGenerator.java
@@ -1,55 +1,69 @@
 public class MapGenerator {
     private Room[][] roomMap;
-    private Room start;
-    private Room end;
+    private Room start = new Room(0);
+    private Room end = new Room(4);
     private Room currentRoom;
 
     public MapGenerator() {
         int roomWidth = (int) (Math.random() * 6 + 10);
         int roomHeight = (int) (Math.random() * 6 + 10);
-        roomMap = (Room[][]) new Object[roomHeight][roomWidth];
+        int startCoord = (int) (Math.random() * (roomWidth - 2)) + 1;
+        int endCoord = (int) (Math.random() * (roomWidth - 2)) + 1;
+        roomMap = (Room[][]) new Room[roomHeight][roomWidth];
+        currentRoom = start;
+        
         for (int i = 0; i < roomHeight; i++) {
             for (int j = 0; j < roomWidth; j++) {
-                roomMap[i][j] = new Room();
+                if (i == 1 && j == startCoord) {
+                    roomMap[i][j] = new Room(0);
+                } else if (i == roomHeight - 2 && j == endCoord) {
+                    roomMap[i][j] = new Room(4);
+                } else {
+                    roomMap[i][j] = new Room();
+                }
             }
         }
-        int startCoord = (int)(Math.random() * (roomWidth - 2)) + 1;
-        start = roomMap[1][startCoord];
-        Room endConnector = roomMap[roomHeight - 1][(int)(Math.random() * (roomWidth - 2)) + 1];
-        Room end = new Room();
-        endConnector.setDown(end);
-        end.setUp(endConnector);
-        int nullCount = 8;
-        for (int i = 3; i < roomHeight - 2; i++) {
-            for (int j = 0; j < roomWidth; j++) {
-                if (roomMap[i][j] != start || roomMap[i][j] != end || roomMap[i][j] != endConnector) {
-                    if (nullCount > 0 && Math.random() * 10 > 0.8) {
-                        roomMap[i][j] = null;
-                        nullCount--;
+        //you must set room adjacents in udlr order!
+        for (int i = 0; i < roomHeight - 1; i++) {
+            for (int j = 0; j < roomWidth - 1; j++) {
+                if (i - 1 >= 0) {
+                    try {
+                        roomMap[i][j].setUp(roomMap[i - 1][j]);
+                    } catch (NullPointerException npe) {
+                        roomMap[i][j].setUp(null);
+                    }
+                }
+                if (i + 1 < roomHeight - 1) {
+                    try {
+                        roomMap[i][j].setDown(roomMap[i + 1][j]);
+                    } catch (NullPointerException npe) {
+                        roomMap[i][j].setDown(null);
+                    }
+                }
+                if (j - 1 >= 0) {
+                    try {
+                        roomMap[i][j].setLeft(roomMap[i][j - 1]);
+                    } catch (NullPointerException npe) {
+                        roomMap[i][j].setLeft(null);
+                    }
+                }
+                if (j + 1 < roomWidth - 1) {
+                    try {
+                        roomMap[i][j].setRight(roomMap[i][j + 1]);
+                    } catch (NullPointerException npe) {
+                        roomMap[i][j].setRight(null);
                     }
                 }
             }
         }
-        currentRoom = start;
-        for (int i = 0; i < roomHeight; i++) {
-            for (int j = 0; j < roomWidth; j++) {
-                if (i - 1 >= 0 && roomMap[i - 1][j] != null) {
-                    roomMap[i][j].setUp(roomMap[i - 1][j]);
-                }
-                if (i + 1 < roomHeight && roomMap[i + 1][j] != null) {
-                    roomMap[i][j].setDown(roomMap[i + 1][j]);
-                }
-                if (j - 1 >= 0 && roomMap[i][j - 1] != null) {
-                    roomMap[i][j].setLeft(roomMap[i][j - 1]);
-                }
-                if (j + 1 < roomWidth && roomMap[i][j + 1] != null) {
-                    roomMap[i][j].setRight(roomMap[i][j + 1]);
-                }
-            }
-        }
+        start = roomMap[1][startCoord];
+        end = roomMap[roomHeight - 2][endCoord];
+        start.setRoomVariant("room_start");
+        end.setRoomVariant("room_exit");
+        end.setExit(true);
     }
-    public Room gameStart() {
-        //initial player coordinates
-        return current;
+
+    public Room getStart() {
+        return start;
     }
 }
